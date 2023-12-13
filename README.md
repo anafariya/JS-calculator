51f700d51fafc3cebd04ca0b53a240dcf0f681a1
Ana Fariya <anafariya@gmail.com>
2023-12-13 16:28:53 +0530

    Added functionality and Read Me file

---
+++ README.md
@@ -0,0 +1,40 @@
+# Calculator App
+
+A simple calculator web application built with HTML, CSS, and JavaScript. This calculator allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.
+
+## Features
+
+- **Clear**: Clears the current input and resets the calculator.
+- **Delete**: Removes the last digit from the current input.
+- **Number Buttons**: Allows users to input numbers.
+- **Operation Buttons**: Selects the desired arithmetic operation.
+- **Equals Button**: Computes the result of the expression.
+- **Display**: Shows the current and previous operands along with the selected operation.
+
+## Usage
+
+1. Click on number buttons to input digits.
+2. Click on operation buttons to select the desired operation.
+3. Press the equals button to calculate the result.
+4. Use the delete button to remove the last digit.
+5. Click on the clear button to reset the calculator.
+
+## Calculator Class
+
+The calculator functionality is implemented using a JavaScript class called `Calculator`. It includes methods for clearing, deleting, appending numbers, choosing operations, and computing results. The display is updated dynamically as users interact with the calculator.
+
+### Calculator Methods
+
+- `clear()`: Clears the calculator state.
+- `delete()`: Removes the last digit from the current input.
+- `appendNumber(number)`: Appends a number to the current input.
+- `chooseOperation(operation)`: Selects an arithmetic operation.
+- `compute()`: Computes the result of the expression.
+- `getDisplayNumber(number)`: Formats the display of numbers.
+
+## Getting Started
+
+1. Clone the repository.
+2. Open `index.html` in a web browser.
+
+Feel free to explore and use this calculator app for your basic arithmetic calculations!
---
+++ script.js
@@ -0,0 +1,124 @@
+class Calculator {
+  constructor(previousOperandTxt, currentOperandTxt) {
+    this.previousOperandTxt = previousOperandTxt;
+    this.currentOperandTxt = currentOperandTxt;
+    this.clear();
+  }
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== " ") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if(isNaN(integerDigits)) {
+        integerDisplay =''
+    }
+    else{
+        integerDisplay = integerDigits.toLocaleString('en',{
+            maximumFractionDigits: 0 })
+    }
+    if(decimalDigits != null){
+        return `${integerDisplay}.${decimalDigits}`
+    }
+    else{
+        return integerDisplay
+    }
+  }
+
+  updateDisplay() {
+    this.currentOperandTxt.innerText = this.getDisplayNumber(this.currentOperand)
+
+    if (this.operation != null) {
+      this.previousOperandTxt.innerText = 
+      `${this.previousOperand} ${this.operation}`
+    }
+    else{
+        this.previousOperandTxt.innerText=""
+    }
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTxt = document.querySelector("[data-prev-operand]");
+const currentOperandTxt = document.querySelector("[data-curr-operand]");
+
+const calculator = new Calculator(previousOperandTxt, currentOperandTxt);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (buttion) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", (buttion) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", (buttion) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
